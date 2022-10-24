20427791f82e75fed747f2c9b5eb43d3b3029f45
Asghar Abbas <101616115+Abbasjafri-syed@users.noreply.github.com>
2022-10-24 07:42:18 +0500

    Blacklistisng multiple accounts

--- Basics/Task-2.sol
+++ Basics/Task-2.sol
@@ -1,20 +1,14 @@
 // SPDX-License-Identifier: No License
 pragma solidity ^0.8.14;
 
-contract abc{
+contract Blacklist{
 
     address public owner;
-    address[] accounts;
-   
+    address[] accounts_blacklisted;
     mapping (address => bool) _blacklisted;
-
+   
     constructor(){
         owner = msg.sender;
-    }
-
-// Blacklist single address
- function blackList(address _addr) internal {
-        _blacklisted[_addr] = true;
     }
   
 // Blacklist multiple address using loop
@@ -22,17 +16,18 @@ contract abc{
 function bulk_blacklisted(address[] memory users) internal {
        for (uint256 i=0; i < users.length; i++){
         _blacklisted[users[i]] =  true;
-        // users[i].push(accounts);  // need solution
-         }
+        unchecked{
+        accounts_blacklisted.push(users[i]);
+         }         }
     }
 
-    // Adding address from external function
+    // Adding addresses into blacklist Array from external function
     function add_blackList(address[] memory _addr)external{
         require(msg.sender == owner, 'Owner Function only');
         bulk_blacklisted(_addr);
     }
 
-    // checking adddress status either blacklisted or not
+    // checking adddress status either "blacklisted" or not
     function check_status(address addr) external view returns (bool){
         if(_blacklisted[addr]){
             return true;
@@ -40,6 +35,12 @@ function bulk_blacklisted(address[] memory users) internal {
         else{
             return false;
         }
-        // (_blacklisted[addr] == true ? return true :  return false ); //need solution
+        // (_blacklisted[addr] == true ? true : false); //need solution
          }
-} 
+
+    // Checking number of accounts in blacklist array
+    function Total_Addressesblacklisted() external view returns (uint){
+        return accounts_blacklisted.length;
+    }
+
+}
